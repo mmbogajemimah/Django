6c29a97d6e51ce5bf74d9ed4d88dff34eca65379
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-06-24 12:36:27 +0300

    Core app

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -6,16 +6,20 @@ from datetime import datetime
 User = get_user_model()
 
 # Create your models here.
+
+
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    profileimg = models.ImageField(
+        upload_to='profile_images', default='blank-profile-picture.png')
     location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.user.username
-    
+
+
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.CharField(max_length=100)
